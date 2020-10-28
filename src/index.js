1228c70692ca7819753645c297a4926bdfdd30f9
Ivan Makarenko <shketiam@gmail.com>
2020-10-28 13:52:12 +0200

    Fix clear of captions

--- src/index.js
+++ src/index.js
@@ -190,7 +190,9 @@ export default class ImageTool {
    */
   save() {
     const caption = this.ui.nodes.caption;
+    caption.innerHTML = caption.innerHTML.replace('<br>', '');
     const subCaption = this.ui.nodes.subCaption;
+    subCaption.innerHTML = subCaption.innerHTML.replace('<br>', '');
 
     this._data.caption = caption.innerHTML;
     this._data.subCaption = subCaption.innerHTML;
